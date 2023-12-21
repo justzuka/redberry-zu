b573813380904a169c6267f7e90bd17066f2b3ed
justzuka <kenchuashvili14@gmail.com>
2023-12-21 18:24:59 +0400

    changed design of the scrollbar and added fade on the sides of the categories

--- redberry-zu/src/Components/Filter/Filter.js
+++ redberry-zu/src/Components/Filter/Filter.js
@@ -6,15 +6,19 @@ import { FilterItem } from "./FilterItem";
 export const Filter = () => {
   const { categories } = useAppContext();
   return (
-    <div className="filter-container">
-      {categories.map((category, index) => (
-        <FilterItem
-          key={category.id}
-          backgroundColor={category.background_color}
-          textColor={category.text_color}
-          title={category.title}
-        />
-      ))}
+    <div className="main-filter-container">
+      <div className="filter-container">
+        {categories.map((category, index) => (
+          <FilterItem
+            key={category.id}
+            backgroundColor={category.background_color}
+            textColor={category.text_color}
+            title={category.title}
+          />
+        ))}
+      </div>
+      <div className="left-gradient-div"></div>
+      <div className="right-gradient-div"></div>
     </div>
   );
 };
